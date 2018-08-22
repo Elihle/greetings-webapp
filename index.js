cd7ae5377a5c37c8169b5a55720c200a95cdf03f
Lihle <lihlemenz@gmail.com>
2018-08-22 12:16:45 +0200

    added greet route with params on index file

--- index.js
+++ index.js
@@ -20,22 +20,26 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   let count = greetMe.countAllGreets();
-  res.render('home', {
-    count
-  });
+  res.render('home', {count});
 });
 
 app.post('/greet', function (req, res) {
-  let greeted = req.body.input;
-  let input = req.body.language;
-  let greetName = greetMe.allGreetings(input, greeted);
+  let name = req.body.name;
+  let language = req.body.language;
+  let greetName = greetMe.allGreetings(language, name);
   let count = greetMe.countAllGreets();
 
-  res.render('home', {
-    greetName,
-    count
-  });
-  console.log(greeted)
+  res.render('home', {greetName,count});
+});
+
+app.get('/greet/:name/:language', function (req, res) {
+  let name = req.params.name;
+  let language = req.params.language;
+  let greetName = greetMe.allGreetings(language, name);
+  let count = greetMe.countAllGreets();
+  console.log(req.params);
+  
+  res.render('home', {greetName, count});
 });
 
 let PORT = process.env.PORT || 3007;
