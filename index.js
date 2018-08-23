dfa40b23096968cc70fcae5e4f057ce720051560
Lihle <lihlemenz@gmail.com>
2018-08-23 17:09:47 +0200

    started with PostgreSQL

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ const bodyParser = require('body-parser');
 let app = express();
 let greetMe = Greetings();
 
+// should we use a SSL connection
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local){
+    useSSL = true;
+}
+// which db connection to use
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:coder@localhost:5432/greetings';
+
+const pool = new Pool({
+    connectionString,
+    ssl : useSSL
+  });
+
+const app = express();
+app.use(session({
+    secret: 'keyboard cat5 run all 0v3r',
+    resave: false,
+    saveUninitialized: true
+}));
+
+//handlebars
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
@@ -40,6 +62,15 @@ app.get('/greet/:name/:language', function (req, res) {
   console.log(req.params);
   
   res.render('home', {greetName, count});
+});
+
+app.post('/add', async function(req, res) {
+  let personName = req.body.greeted_names;
+  if (personName && personName !== '') {
+      await pool.query('insert into greeted (greeted_names, spotted_greetings) values ($1, $2)' , [personName, 1]);    
+  }
+
+  res.redirect('/');
 });
 
 let PORT = process.env.PORT || 3007;
---
+++ table.sql
@@ -0,0 +1,5 @@
+create table greetings(
+	id serial not null primary key,
+    greeted_names text not null,
+    spotted_greetings int not null
+);
