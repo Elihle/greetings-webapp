52557fd83c272a014f8c6612d29c303e402e4b55
Lihle <lihlemenz@gmail.com>
2018-09-04 16:25:43 +0200

    added new route on index.js

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 let greetMe = Greetings();
-const session = require('express-session');
 const pg = require("pg");
 const Pool = pg.Pool;
 // should we use a SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local) {
-  useSSL = true;
+if (process.env.DATABASE_URL && !local){
+    useSSL = true;
 }
+
+const flash = require('express-flash');
+const session = require('express-session');
+
 // which db connection to use
 const connectionString = process.env.DATABASE_URL || 'postgresql://coder:coder@localhost:5432/greetings';
 
@@ -52,11 +55,12 @@ app.post('/greet', async function (req, res) {
   let name = req.body.name;
   let language = req.body.language;
   let greetName = greetMe.allGreetings(language, name);
+
   let user = await pool.query('select * from greetings where greeted_names = $1', [name]);
   if (user.rows.length != 0){
-    let spottedCount = user.rows[0].spotted_greetings;
-    spottedCount++;
-    await pool.query('UPDATE greetings SET spotted_greetings = $1 where greeted_names = $2', [spottedCount, name]);
+  let spottedCount = user.rows[0].spotted_greetings;
+  spottedCount++;
+  await pool.query('UPDATE greetings SET spotted_greetings = $1 where greeted_names = $2', [spottedCount, name]);
   }
   else {
     await pool.query('INSERT INTO greetings (greeted_names, spotted_greetings) values ($1, $2)', [name, 1]);
@@ -75,18 +79,17 @@ app.get('/greet/:name/:language', async function (req, res) {
 
   let user = await pool.query('select * from greetings where greeted_names = $1', [name]);
   if (user.rows.length != 0){
-    let spottedCount = user.rows[0].spotted_greetings;
-    spottedCount++;
-    await pool.query('UPDATE greetings SET spotted_greetings = $1 where greeted_names = $2', [spottedCount, name]);
+  let spottedCount = user.rows[0].spotted_greetings;
+  spottedCount++;
+  await pool.query('UPDATE greetings SET spotted_greetings = $1 where greeted_names = $2', [spottedCount, name]);
   }
   else {
-    await pool.query('INSERT INTO greetings (greeted_names, spotted_greetings) values ($1, $2)', [name, 1]);
+  await pool.query('INSERT INTO greetings (greeted_names, spotted_greetings) values ($1, $2)', [name, 1]);
   }
   let counting = await pool.query('select count(*) from greetings');
   let databaseLength = counting.rows[0].count;
 
-
-  res.render('home', {greetName, databaseLength});
+res.render('home', {greetName, databaseLength, user});
 });
 
 //keep in database
@@ -98,11 +101,13 @@ app.get('/greeted', async function (req, res) {
   } catch (err) {
     res.send(err.stack);
   }
-  let greetName = req.body.greeted_names;
-  // if (greetName && greetName !== '') {
-  //   await pool.query('insert into database (greeted_name, spotted_greetings) values ($1, $2)', [greetName, 1]);
-  // }
-  console.log(greetName);
+});
+
+  app.get('/add/:name', async function(req, res) {
+    let username = req.params.name;
+    let counting = await pool.query('select * from greetings where greeted_names = $1', [username]);
+    let count = counting.rows[0].spotted_greetings;
+    res.render('add', {count, username});
 });
 
 let PORT = process.env.PORT || 3007;
